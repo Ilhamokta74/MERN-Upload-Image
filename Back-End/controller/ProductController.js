79f6852f1b04b799aa9e6b7048bf091ea9878961
Ilham Oktavian <99379666+Ilhamokta74@users.noreply.github.com>
2024-08-01 21:32:30 +0700

    Fix Back-End

--- Back-End/controller/ProductController.js
+++ Back-End/controller/ProductController.js
@@ -1,5 +1,6 @@
 import Product from "../model/ProductModel.js";
 import path from "path";
+import fs from "fs";
 
 export const getProduct = async (req, res) => {
     try {
@@ -49,17 +50,69 @@ export const saveProduct = async (req, res) => {
 }
 
 export const updateProduct = async (req, res) => {
+    const product = await Product.findOne({
+        where: {
+            id: req.params.id
+        }
+    });
+
+    if (!product) return res.status(404).json({ msg: "No Data Found" });
+    let fileName = "";
+    if (req.files === null) {
+        fileName = Product.image;
+    } else {
+        const file = req.files.file;
+        const fileSize = file.data.length;
+        const ext = path.extname(file.name);
+        fileName = file.md5 + ext;
+        const allowedType = ['.png', '.jpg', '.jpeg'];
+
+        if (!allowedType.includes(ext.toLowerCase())) return res.status(422).json({ msg: "Invalid Images" });
+
+        if (fileSize > 5000000) return res.status(422).json({ msg: "Images Must Be Less Then 5 MB" });
+
+        const filepath = `./public/images/${product.image}`;
+        fs.unlinkSync(filepath);
+
+        file.mv(`./public/images/${fileName}`, async (err) => {
+            if (err) return res.status(500).json({ msg: err.message });
+        })
+    }
+    const name = req.body.title;
+    const url = `${req.protocol}://${req.get("host")}/images/${fileName}`;
+
     try {
-
-    } catch (e) {
-
+        await Product.update({ name: name, image: fileName, url: url }, {
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json({ msg: "Product Updated Successfully" });
+    } catch (err) {
+        console.log(err.message);
     }
 }
 
 export const deleteProduct = async (req, res) => {
+    const product = await Product.findOne({
+        where: {
+            id: req.params.id
+        }
+    });
+
+    if (!product) return res.status(404).json({ msg: "No Data Found" });
+
     try {
+        const filepath = `./public/images/${product.image}`;
+        fs.unlinkSync(filepath);
+        await Product.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
 
+        res.status(200).json({ msg: "Product Deleted Successfully" })
     } catch (e) {
-
+        console.log(e.message);
     }
 }
