5602ddd86d17215a0506cdda9344916265f6ba7a
Ilham Oktavian <99379666+Ilhamokta74@users.noreply.github.com>
2024-08-01 20:29:18 +0700

    Database Fix

---
+++ Back-End/controller/ProductController.js
@@ -0,0 +1,21 @@
+import Product from "../model/ProductModel.js";
+
+export const getProduct = async (eq, res) => {
+
+}
+
+export const getProductById = async (eq, res) => {
+    
+}
+
+export const saveProduct = async (eq, res) => {
+    
+}
+
+export const updateProduct = async (eq, res) => {
+    
+}
+
+export const deleteProduct = async (eq, res) => {
+    
+}
--- Back-End/index.js
+++ Back-End/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import fileUpload from "express-fileupload";
 import cors from "cors";
+import ProductRoute from './routes/ProductRoute.js' 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
+app.use(ProductRoute);
 
 app.listen(5000, () => {
     console.log(`Server Running On http://localhost:5000`);
